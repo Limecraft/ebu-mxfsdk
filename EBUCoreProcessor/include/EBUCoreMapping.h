b3ab461f813ed32d08de799a6a25aae2792a5e9d
Dieter Van Rijsselbergen <dieter.vanrijsselbergen@limecraft.com>
2012-07-19 22:44:26 +0200

    Moved mapping functions to separate EBUCoreMapping.cpp and EBUCoreMapping.h to keep EBUCoreProcessor manageable in size.

---
+++ EBUCoreProcessor/include/EBUCoreMapping.h
@@ -0,0 +1,33 @@
+
+#include <libMXF++/MXF.h>
+
+#include "EBU_CORE_20110915.hxx"
+
+#include <metadata/EBUCoreDMS++.h>
+
+namespace EBUCore {
+
+class ObjectModifier {
+public:
+	virtual void Modify(mxfpp::InterchangeObject *obj) = 0;
+};
+
+class GenerationUIDAppender : public ObjectModifier {
+	mxfUUID _generationUID;
+public:
+	GenerationUIDAppender(mxfUUID GenerationUID) {
+		_generationUID = GenerationUID;
+	}
+	void Modify(mxfpp::InterchangeObject *obj) {
+		obj->setGenerationUID(_generationUID);
+	}
+};
+
+void mapCoreMetadata(ebuCore_2011::coreMetadataType& source, ebucoreCoreMetadata *dest, ObjectModifier* mod = NULL);
+void mapCoreMetadata(ebucoreCoreMetadata *source, ebuCore_2011::coreMetadataType& dest);
+void mapMetadataSchemeInformation(ebuCore_2011::ebuCoreMainType& source, ebucoreMetadataSchemeInformation *dest, ObjectModifier* mod = NULL);
+void mapMetadataSchemeInformation(ebucoreMetadataSchemeInformation *source, ebuCore_2011::ebuCoreMainType& dest);
+void mapEntity(ebuCore_2011::entityType& source, ebucoreEntity *dest, ObjectModifier* mod = NULL);
+void mapEntity(ebucoreEntity *source, ebuCore_2011::entityType& dest);
+
+}
