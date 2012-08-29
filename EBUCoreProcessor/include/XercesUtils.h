b8a11b14b67368556eb38b0ddeb1b144a6ccdb5f
Dieter Van Rijsselbergen <dieter.vanrijsselbergen@limecraft.com>
2012-08-29 02:19:10 +0200

    Fixed: improper parsing of XML documents where generated DOMDocuments are released when the parser is released. Now employ a DOMLSParser which can return documents with released ownership (i.e., fgXercesUserAdoptsDOMDocument config flag).

--- EBUCoreProcessor/include/XercesUtils.h
+++ EBUCoreProcessor/include/XercesUtils.h
@@ -2,9 +2,12 @@
 #include <xercesc/sax/InputSource.hpp>
 #include <xercesc/util/BinInputStream.hpp>
 #include <xercesc/sax/InputSource.hpp>
+#include <xercesc/framework/Wrapper4InputSource.hpp>
 #include <xercesc/parsers/XercesDOMParser.hpp>
 #include <xercesc/sax/HandlerBase.hpp>
+#include <xercesc/dom/DOMImplementation.hpp>
 #include <xercesc/dom/DOMImplementationLS.hpp>
+#include <xercesc/dom/DOMLSParser.hpp>
 #include <xercesc/dom/DOMLSSerializer.hpp>
 #include <xercesc/dom/DOMLSOutput.hpp>
 #include <xercesc/dom/DOMImplementationRegistry.hpp>
@@ -41,49 +44,95 @@ public:
 	}
 };
 
-void ConfigureParser(XercesDOMParser* parser, ErrorHandler *errHandler) {
-	parser->setValidationScheme(XercesDOMParser::Val_Never);
-	parser->setValidationSchemaFullChecking(false);
-	parser->setDoSchema(false);
-	parser->setDoNamespaces(true);    // optional
-    parser->setErrorHandler(errHandler);
+class PlainDOMErrorHandler : public DOMErrorHandler {
+	bool handleError (const DOMError &domError) {
+		// do nothing??
+		return false;	// stop processing on errors
+	}
+};
+
+DOMLSParser* ObtainParser(DOMErrorHandler& errHandler) {
+
+	const XMLCh ls_id [] = {chLatin_L, chLatin_S, chNull};
+
+	// Get an implementation of the Load-Store (LS) interface.
+	//
+	DOMImplementation* impl(DOMImplementationRegistry::getDOMImplementation (ls_id));
+
+	DOMLSParser *parser (impl->createLSParser (DOMImplementationLS::MODE_SYNCHRONOUS, 0));
+
+	DOMConfiguration* conf (parser->getDomConfig ());
+
+	// Discard comment nodes in the document.
+	//
+	conf->setParameter (XMLUni::fgDOMComments, false);
+
+	// Enable datatype normalization.
+	//
+	conf->setParameter (XMLUni::fgDOMDatatypeNormalization, true);
+
+	// Do not create EntityReference nodes in the DOM tree. No
+	// EntityReference nodes will be created, only the nodes
+	// corresponding to their fully expanded substitution text
+	// will be created.
+	//
+	conf->setParameter (XMLUni::fgDOMEntities, false);
+
+	// Perform namespace processing.
+	//
+	conf->setParameter (XMLUni::fgDOMNamespaces, true);
+
+	// Do not include ignorable whitespace in the DOM tree.
+	//
+	conf->setParameter (XMLUni::fgDOMElementContentWhitespace, false);
+
+	// Enable/Disable validation.
+	//
+	conf->setParameter (XMLUni::fgDOMValidate, false);
+	conf->setParameter (XMLUni::fgXercesSchema, false);
+	conf->setParameter (XMLUni::fgXercesSchemaFullChecking, false);
+
+	// Xerces-C++ 3.1.0 is the first version with working multi import
+	// support.
+	//
+#if _XERCES_VERSION >= 30100
+	conf->setParameter (XMLUni::fgXercesHandleMultipleImports, true);
+#endif
+
+	// We will release the DOM document ourselves.
+	//
+	conf->setParameter (XMLUni::fgXercesUserAdoptsDOMDocument, true);
+
+	// Set error handler.
+	//
+	conf->setParameter (XMLUni::fgDOMErrorHandler, &errHandler);
+
+	return parser;
 }
 
 DOMDocument* ParseXercesDocument(const XMLCh* location) {
 
-	XercesDOMParser* parser = new XercesDOMParser();
-	ErrorHandler* errHandler = (ErrorHandler*) new HandlerBase();
-	ConfigureParser(parser, errHandler);
-
-	DOMDocument* out = NULL;
-
+	PlainDOMErrorHandler errHandler;
+	std::auto_ptr<DOMLSParser> parser ( ObtainParser(errHandler) );	
+	
 	try {
-		parser->parse(location);
-		out = parser->getDocument();
-	} catch (...) {}
-
-	delete parser;
-	delete errHandler;
-
-	return out;
+		return parser->parseURI(location);
+	} catch (...) {
+		return NULL;
+	}
 }
 
 DOMDocument* ParseXercesDocument(InputSource& input) {
-	XercesDOMParser* parser = new XercesDOMParser();
-	ErrorHandler* errHandler = (ErrorHandler*) new HandlerBase();
-	ConfigureParser(parser, errHandler);
 
-	DOMDocument* out = NULL;
+	PlainDOMErrorHandler errHandler;
+	std::auto_ptr<DOMLSParser> parser ( ObtainParser(errHandler) );	
 
 	try {
-		parser->parse(input);
-		out = parser->getDocument();
-	} catch (...) {}
-
-	delete parser;
-	delete errHandler;
-
-	return out;
+		Wrapper4InputSource wrap(&input, false /* don't adopt the source, it will be deleted upstream */);
+		return parser->parse(&wrap);
+	} catch (...) {
+		return NULL;
+	}
 }
 
 class DarkDOMDocumentSerializer : public EBUCore::MXFFileDarkSerializer, public xercesc::XMLFormatTarget {
