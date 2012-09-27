aa334a81e2e9d13ac0ab66496697da904ce09bbc
Dieter Van Rijsselbergen <dieter.vanrijsselbergen@limecraft.com>
2012-09-27 11:19:53 +0200

    Updated dictionary conversion to local gen_ebucore_classes.

--- EBUCoreProcessor/dictionary/dictconvert.bat
+++ EBUCoreProcessor/dictionary/dictconvert.bat
@@ -1,4 +1,4 @@
 set PATH=c:\code\Java\groovy-1.8.6\bin;%PATH%
-cmd /C groovy dictconvert.groovy ebucore_mxflib_dict.xml > ..\..\..\ebu-libMXFpp\tools\gen_ebucore_classes\ebucore_extensions_data_model.h
-cmd /C groovy declarereferences.groovy ebucore_mxflib_dict.xml > ..\..\..\ebu-libMXFpp\tools\gen_ebucore_classes\ebucore_declare_references.inc
+cmd /C groovy dictconvert.groovy ebucore_mxflib_dict.xml > ..\include\metadata\ebucore_extensions_data_model.h
+cmd /C groovy declarereferences.groovy ebucore_mxflib_dict.xml > ..\..\tools\gen_ebucore_classes\ebucore_declare_references.inc
 pause
