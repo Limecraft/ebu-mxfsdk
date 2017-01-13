5f3a60cadd722a862ffa74fc582d13000203b9c7
Dieter Van Rijsselbergen <dieter.vanrijsselbergen@limecraft.com>
2017-01-13 17:24:27 +0100

    Updated AS11 dictionary location.

--- Analyzer/ST434/generate.sh
+++ Analyzer/ST434/generate.sh
@@ -2,4 +2,4 @@
 find schemas/groups schemas/dictionary -type f -not -name '*ebucore*.xsd' | xargs java -cp "bin;guava-14.0.1.jar" Convert st434 > ../src/group_declarations.inc
 echo schemas/dictionary/class13_ebucore_extension_dictionary.xsd | xargs java -cp "bin;guava-14.0.1.jar" Convert ebucore_elements >> ../src/group_declarations.inc
 java -cp "bin;guava-14.0.1.jar" Dictconvert ../../EBUCoreProcessor/dictionary/ebucore_mxflib_dict.xml >> ../src/group_declarations.inc
-java -cp "bin;guava-14.0.1.jar" Dictconvert_with_elements ../../ebu-bmx/src/as11/as11_mxflib_dict.xml "urn:dpp:metadata:as11" key_ns_as11_1 > ../src/group_declarations_as11.inc
+java -cp "bin;guava-14.0.1.jar" Dictconvert_with_elements ../../ebu-bmx/meta/as11/as11_mxflib_dict.xml "urn:dpp:metadata:as11" key_ns_as11_1 > ../src/group_declarations_as11.inc
