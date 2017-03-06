4c3784ecbe73d8aefabc366192a3b9118d45bbf2
Dieter Van Rijsselbergen <dieter.vanrijsselbergen@limecraft.com>
2017-03-06 17:20:08 +0100

    Make sure we always return something.

--- EBUCoreProcessor/src/EBUCoreProcessor.cpp
+++ EBUCoreProcessor/src/EBUCoreProcessor.cpp
@@ -1138,6 +1138,8 @@ MetadataKind ExtractEBUCoreMetadata(
             }
             return RP2057;
         }
+        
+        return NONE;
     }
 	else {
 		// ///////////////////////////////////////
